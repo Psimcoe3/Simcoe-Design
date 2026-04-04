525c509e6f0b94d9bae063d6bbce24e6d570d3cc
Psimcoe3 <psimcoe3@gmail.com>
2026-04-03 20:54:47 -0400

    Add measurement direct drag coverage

--- ElectricalComponentSandbox.Tests/MainWindowMarkupInteractionTests.DirectDrag.cs
+++ ElectricalComponentSandbox.Tests/MainWindowMarkupInteractionTests.DirectDrag.cs
@@ -240,6 +240,42 @@ public partial class MainWindowMarkupInteractionTests
     }
 
     [Fact]
+    public void DirectVertexDragForTesting_LineStyleMeasurement_UsesPolarSnapAndSupportsUndo()
+    {
+        var outcome = RunWithSelectedMarkupWindow(
+            new MarkupRecord
+            {
+                Type = MarkupType.Measurement,
+                Vertices = { new Point(0, 0), new Point(10, 0), new Point(30, 20) },
+                Metadata = new MarkupMetadata { Subject = "Diameter" }
+            },
+            (window, viewModel, markup) =>
+            {
+                var began = window.BeginSelectedMarkupVertexDragForTesting(new Point(10, 0));
+                window.UpdateDraggedMarkupVertexPreviewForTesting(new Point(18, 6));
+                window.FinishMarkupVertexDragForTesting();
+                var editedState = (markup.Vertices[1], markup.Vertices[2]);
+
+                viewModel.Undo();
+                var undoneState = (markup.Vertices[1], markup.Vertices[2]);
+                return (began, editedState, undoneState);
+            },
+            viewModel =>
+            {
+                viewModel.SnapToGrid = false;
+                viewModel.IsPolarActive = true;
+                viewModel.PolarIncrementDeg = 30;
+            });
+
+        Assert.True(outcome.began);
+        Assert.Equal(16.431676725154983, outcome.editedState.Item1.X, 3);
+        Assert.Equal(9.486832980505136, outcome.editedState.Item1.Y, 3);
+        Assert.NotEqual(new Point(30, 20), outcome.editedState.Item2);
+        Assert.Equal(new Point(10, 0), outcome.undoneState.Item1);
+        Assert.Equal(new Point(30, 20), outcome.undoneState.Item2);
+    }
+
+    [Fact]
     public void DirectVertexDragForTesting_PathMarkup_SnapsToSiblingVertexAndSupportsUndo()
     {
         var outcome = RunWithSelectedMarkupWindow(
@@ -523,6 +559,43 @@ public partial class MainWindowMarkupInteractionTests
     }
 
     [Fact]
+    public void DirectArcAngleDragForTesting_AngularMeasurement_UpdatesGeometryAndSupportsUndo()
+    {
+        var outcome = RunWithSelectedMarkupWindow(
+            new MarkupRecord
+            {
+                Type = MarkupType.Measurement,
+                Vertices = { new Point(0, 0), new Point(10, 0), new Point(0, 10), new Point(8, 8) },
+                Radius = 8,
+                ArcStartDeg = 0,
+                ArcSweepDeg = 90,
+                Metadata = new MarkupMetadata { Subject = "Angular" }
+            },
+            (window, viewModel, markup) =>
+            {
+                var began = window.BeginSelectedMarkupArcAngleDragForTesting(new Point(0, 10));
+                window.UpdateDraggedMarkupArcAnglePreviewForTesting(new Point(10, 10));
+                window.FinishMarkupArcAngleDragForTesting();
+                var editedState = (markup.Vertices[2], markup.ArcSweepDeg);
+
+                viewModel.Undo();
+                var undoneState = (markup.Vertices[2], markup.ArcSweepDeg);
+                return (began, editedState, undoneState);
+            },
+            viewModel =>
+            {
+                viewModel.SnapToGrid = false;
+            });
+
+        Assert.True(outcome.began);
+        Assert.Equal(7.0710678118654755, outcome.editedState.Item1.X, 6);
+        Assert.Equal(7.0710678118654755, outcome.editedState.Item1.Y, 6);
+        Assert.Equal(45, outcome.editedState.Item2, 6);
+        Assert.Equal(new Point(0, 10), outcome.undoneState.Item1);
+        Assert.Equal(90, outcome.undoneState.Item2, 6);
+    }
+
+    [Fact]
     public void DirectArcAngleDragForTesting_AngularDimension_UsesPolarSnapAndSupportsUndo()
     {
         var outcome = RunWithSelectedMarkupWindow(
@@ -561,12 +634,89 @@ public partial class MainWindowMarkupInteractionTests
     }
 
     [Fact]
+    public void DirectArcAngleDragForTesting_AngularMeasurement_UsesPolarSnapAndSupportsUndo()
+    {
+        var outcome = RunWithSelectedMarkupWindow(
+            new MarkupRecord
+            {
+                Type = MarkupType.Measurement,
+                Vertices = { new Point(0, 0), new Point(10, 0), new Point(0, 10), new Point(8, 8) },
+                Radius = 8,
+                ArcStartDeg = 0,
+                ArcSweepDeg = 90,
+                Metadata = new MarkupMetadata { Subject = "Angular" }
+            },
+            (window, viewModel, markup) =>
+            {
+                var began = window.BeginSelectedMarkupArcAngleDragForTesting(new Point(0, 10));
+                window.UpdateDraggedMarkupArcAnglePreviewForTesting(new Point(6, 8));
+                window.FinishMarkupArcAngleDragForTesting();
+                var editedState = (markup.Vertices[2], markup.ArcSweepDeg);
+
+                viewModel.Undo();
+                var undoneState = (markup.Vertices[2], markup.ArcSweepDeg);
+                return (began, editedState, undoneState);
+            },
+            viewModel =>
+            {
+                viewModel.IsPolarActive = true;
+                viewModel.PolarIncrementDeg = 30;
+            });
+
+        Assert.True(outcome.began);
+        Assert.Equal(5, outcome.editedState.Item1.X, 6);
+        Assert.Equal(8.660254037844386, outcome.editedState.Item1.Y, 6);
+        Assert.Equal(60, outcome.editedState.Item2, 6);
+        Assert.Equal(new Point(0, 10), outcome.undoneState.Item1);
+        Assert.Equal(90, outcome.undoneState.Item2, 6);
+    }
+
+    [Fact]
     public void DirectRadiusDragForTesting_ArcLengthDimension_UpdatesGeometryAndSupportsUndo()
     {
         var outcome = RunWithSelectedMarkupWindow(
             new MarkupRecord
             {
                 Type = MarkupType.Dimension,
+                Vertices = { new Point(10, 0), new Point(0, 10), new Point(7.0710678118654755, 7.0710678118654755) },
+                Radius = 10,
+                ArcStartDeg = 0,
+                ArcSweepDeg = 90,
+                Metadata = new MarkupMetadata { Subject = "ArcLength" }
+            },
+            (window, viewModel, markup) =>
+            {
+                var began = window.BeginSelectedMarkupRadiusDragForTesting(new Point(7.0710678118654755, 7.0710678118654755));
+                window.UpdateDraggedMarkupRadiusPreviewForTesting(new Point(8.48528137423857, 8.48528137423857));
+                window.FinishMarkupRadiusDragForTesting();
+                var editedState = (markup.Vertices[1], markup.Radius, markup.ArcSweepDeg);
+
+                viewModel.Undo();
+                var undoneState = (markup.Vertices[1], markup.Radius, markup.ArcSweepDeg);
+                return (began, editedState, undoneState);
+            },
+            viewModel =>
+            {
+                viewModel.SnapToGrid = false;
+            });
+
+        Assert.True(outcome.began);
+        Assert.Equal(3.105828541230249, outcome.editedState.Item1.X, 6);
+        Assert.Equal(11.59110991546882, outcome.editedState.Item1.Y, 6);
+        Assert.Equal(12, outcome.editedState.Item2, 6);
+        Assert.Equal(75, outcome.editedState.Item3, 6);
+        Assert.Equal(new Point(0, 10), outcome.undoneState.Item1);
+        Assert.Equal(10, outcome.undoneState.Item2, 6);
+        Assert.Equal(90, outcome.undoneState.Item3, 6);
+    }
+
+    [Fact]
+    public void DirectRadiusDragForTesting_ArcLengthMeasurement_UpdatesGeometryAndSupportsUndo()
+    {
+        var outcome = RunWithSelectedMarkupWindow(
+            new MarkupRecord
+            {
+                Type = MarkupType.Measurement,
                 Vertices = { new Point(10, 0), new Point(0, 10), new Point(7.0710678118654755, 7.0710678118654755) },
                 Radius = 10,
                 ArcStartDeg = 0,
@@ -674,12 +824,86 @@ public partial class MainWindowMarkupInteractionTests
     }
 
     [Fact]
+    public void DirectRadiusDragForTesting_AngularMeasurement_UsesGridSnapAndSupportsUndo()
+    {
+        var outcome = RunWithSelectedMarkupWindow(
+            new MarkupRecord
+            {
+                Type = MarkupType.Measurement,
+                Vertices = { new Point(0, 0), new Point(10, 0), new Point(0, 10), new Point(8, 8) },
+                Radius = 8,
+                ArcStartDeg = 0,
+                ArcSweepDeg = 90,
+                Metadata = new MarkupMetadata { Subject = "Angular" }
+            },
+            (window, viewModel, markup) =>
+            {
+                var began = window.BeginSelectedMarkupRadiusDragForTesting(new Point(5.656854249492381, 5.65685424949238));
+                window.UpdateDraggedMarkupRadiusPreviewForTesting(new Point(11, 11));
+                window.FinishMarkupRadiusDragForTesting();
+                var editedState = (markup.Radius, markup.ArcSweepDeg);
+
+                viewModel.Undo();
+                var undoneState = (markup.Radius, markup.ArcSweepDeg);
+                return (began, editedState, undoneState);
+            },
+            viewModel =>
+            {
+                viewModel.SnapToGrid = true;
+                viewModel.GridSize = 1.0;
+            });
+
+        Assert.True(outcome.began);
+        Assert.Equal(28.284271247461902, outcome.editedState.Item1, 6);
+        Assert.Equal(90, outcome.editedState.Item2, 6);
+        Assert.Equal(8, outcome.undoneState.Item1, 6);
+        Assert.Equal(90, outcome.undoneState.Item2, 6);
+    }
+
+    [Fact]
     public void DirectRadiusDragForTesting_ArcLengthDimension_UsesGridSnapAndSupportsUndo()
     {
         var outcome = RunWithSelectedMarkupWindow(
             new MarkupRecord
             {
                 Type = MarkupType.Dimension,
+                Vertices = { new Point(10, 0), new Point(0, 10), new Point(7.0710678118654755, 7.0710678118654755) },
+                Radius = 10,
+                ArcStartDeg = 0,
+                ArcSweepDeg = 90,
+                Metadata = new MarkupMetadata { Subject = "ArcLength" }
+            },
+            (window, viewModel, markup) =>
+            {
+                var began = window.BeginSelectedMarkupRadiusDragForTesting(new Point(7.0710678118654755, 7.0710678118654755));
+                window.UpdateDraggedMarkupRadiusPreviewForTesting(new Point(11, 11));
+                window.FinishMarkupRadiusDragForTesting();
+                var editedState = (markup.Radius, markup.ArcSweepDeg);
+
+                viewModel.Undo();
+                var undoneState = (markup.Radius, markup.ArcSweepDeg);
+                return (began, editedState, undoneState);
+            },
+            viewModel =>
+            {
+                viewModel.SnapToGrid = true;
+                viewModel.GridSize = 1.0;
+            });
+
+        Assert.True(outcome.began);
+        Assert.Equal(28.284271247461902, outcome.editedState.Item1, 6);
+        Assert.Equal(31.819805153394636, outcome.editedState.Item2, 6);
+        Assert.Equal(10, outcome.undoneState.Item1, 6);
+        Assert.Equal(90, outcome.undoneState.Item2, 6);
+    }
+
+    [Fact]
+    public void DirectRadiusDragForTesting_ArcLengthMeasurement_UsesGridSnapAndSupportsUndo()
+    {
+        var outcome = RunWithSelectedMarkupWindow(
+            new MarkupRecord
+            {
+                Type = MarkupType.Measurement,
                 Vertices = { new Point(10, 0), new Point(0, 10), new Point(7.0710678118654755, 7.0710678118654755) },
                 Radius = 10,
                 ArcStartDeg = 0,
@@ -748,12 +972,87 @@ public partial class MainWindowMarkupInteractionTests
     }
 
     [Fact]
+    public void DirectArcAngleDragForTesting_ArcLengthMeasurement_UpdatesGeometryAndSupportsUndo()
+    {
+        var outcome = RunWithSelectedMarkupWindow(
+            new MarkupRecord
+            {
+                Type = MarkupType.Measurement,
+                Vertices = { new Point(10, 0), new Point(0, 10), new Point(7.0710678118654755, 7.0710678118654755) },
+                Radius = 10,
+                ArcStartDeg = 0,
+                ArcSweepDeg = 90,
+                Metadata = new MarkupMetadata { Subject = "ArcLength" }
+            },
+            (window, viewModel, markup) =>
+            {
+                var began = window.BeginSelectedMarkupArcAngleDragForTesting(new Point(0, 10));
+                window.UpdateDraggedMarkupArcAnglePreviewForTesting(new Point(-10, 0));
+                window.FinishMarkupArcAngleDragForTesting();
+                var editedState = (markup.Vertices[1], markup.ArcSweepDeg);
+
+                viewModel.Undo();
+                var undoneState = (markup.Vertices[1], markup.ArcSweepDeg);
+                return (began, editedState, undoneState);
+            },
+            viewModel =>
+            {
+                viewModel.SnapToGrid = false;
+            });
+
+        Assert.True(outcome.began);
+        Assert.Equal(-10, outcome.editedState.Item1.X, 6);
+        Assert.Equal(0, outcome.editedState.Item1.Y, 6);
+        Assert.Equal(180, outcome.editedState.Item2, 6);
+        Assert.Equal(new Point(0, 10), outcome.undoneState.Item1);
+        Assert.Equal(90, outcome.undoneState.Item2, 6);
+    }
+
+    [Fact]
     public void DirectArcAngleDragForTesting_ArcLengthDimension_UsesPolarSnapAndSupportsUndo()
     {
         var outcome = RunWithSelectedMarkupWindow(
             new MarkupRecord
             {
                 Type = MarkupType.Dimension,
+                Vertices = { new Point(10, 0), new Point(0, 10), new Point(7.0710678118654755, 7.0710678118654755) },
+                Radius = 10,
+                ArcStartDeg = 0,
+                ArcSweepDeg = 90,
+                Metadata = new MarkupMetadata { Subject = "ArcLength" }
+            },
+            (window, viewModel, markup) =>
+            {
+                var began = window.BeginSelectedMarkupArcAngleDragForTesting(new Point(0, 10));
+                window.UpdateDraggedMarkupArcAnglePreviewForTesting(new Point(6, 8));
+                window.FinishMarkupArcAngleDragForTesting();
+                var editedState = (markup.Vertices[1], markup.ArcSweepDeg);
+
+                viewModel.Undo();
+                var undoneState = (markup.Vertices[1], markup.ArcSweepDeg);
+                return (began, editedState, undoneState);
+            },
+            viewModel =>
+            {
+                viewModel.IsPolarActive = true;
+                viewModel.PolarIncrementDeg = 30;
+            });
+
+        Assert.True(outcome.began);
+        Assert.Equal(5, outcome.editedState.Item1.X, 6);
+        Assert.Equal(8.660254037844386, outcome.editedState.Item1.Y, 6);
+        Assert.Equal(60, outcome.editedState.Item2, 6);
+        Assert.Equal(new Point(0, 10), outcome.undoneState.Item1);
+        Assert.Equal(90, outcome.undoneState.Item2, 6);
+    }
+
+    [Fact]
+    public void DirectArcAngleDragForTesting_ArcLengthMeasurement_UsesPolarSnapAndSupportsUndo()
+    {
+        var outcome = RunWithSelectedMarkupWindow(
+            new MarkupRecord
+            {
+                Type = MarkupType.Measurement,
                 Vertices = { new Point(10, 0), new Point(0, 10), new Point(7.0710678118654755, 7.0710678118654755) },
                 Radius = 10,
                 ArcStartDeg = 0,
